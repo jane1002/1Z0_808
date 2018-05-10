eaed515d175afa055e596e868333f5405416aced
Tyamosrgo <1012733649@qq.com>
2018-05-10 23:35:56 +0800

    18/5/10更新
    添加至Q108

--- src/ch03/Q101.java
+++ src/ch03/Q101.java
@@ -1,5 +1,20 @@
 package ch03;
+//import java.io.*;
 
 public class Q101 {
-
+		public static void main(String[] args) {
+			String str1 =new String("123456789");
+			str1.substring(0,3);
+			System.out.println(str1);
+			
+			
+			StringBuilder str2=new StringBuilder("123456789");
+			str2.substring(0,3);
+			str2.append("XX");
+			
+			System.out.println(str2);
+			
+			
+			
+		}
 }
